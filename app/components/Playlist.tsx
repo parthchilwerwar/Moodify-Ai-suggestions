dff676a84983110b3f52feea0fb8bd5fba41e7bc
parthchilwerwar <sudo.jaxkarth@gmail.com>
2024-09-29 16:22:14 +0530

    updated ring

--- app/components/Playlist.tsx
+++ app/components/Playlist.tsx
@@ -45,7 +45,7 @@ export default function Playlist({ tracks }: PlaylistProps) {
               href={`https://www.youtube.com/results?search_query=${track.searchQuery}`}
               target="_blank" 
               rel="noopener noreferrer"
-              className="ml-2 text-blue-neon opacity-75 hover:opacity-100 transition duration-300"
+              className="ml-2 text-blue-600  hover:opacity-100 transition duration-300"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 md:h-5 md:w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
